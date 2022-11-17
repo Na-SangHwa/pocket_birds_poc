6aa8937559e107c818eb7eab41f604d1ff2291e3
Na-sanghwa <8008165@gmail.com>
2022-11-18 03:42:28 +0900

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,106 @@
+import 'dart:io';
+import 'package:image/image.dart' as img;
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+import 'package:pocket_birds_poc/classifier.dart';
+import 'package:pocket_birds_poc/classifier_quant.dart';
+
+import 'package:logger/logger.dart';
+import 'package:tflite_flutter_helper/tflite_flutter_helper.dart';
+
+
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, this.title}) : super(key: key);
+
+  final String? title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Classifier _classifier;
+
+  var logger = Logger();
+
+  File? _image;
+  final picker = ImagePicker();
+
+  Image? _imageWidget;
+
+  img.Image? fox;
+
+  Category? category;
+
+  @override
+  void initState() {
+    super.initState();
+    _classifier = ClassifierQuant();
+  }
+
+  Future getImage() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      _image = File(pickedFile!.path);
+      _imageWidget = Image.file(_image!);
+
+      _predict();
+    });
+  }
+
+  void _predict() async {
+    img.Image imageInput = img.decodeImage(_image!.readAsBytesSync())!;
+    var pred = _classifier.predict(imageInput);
+
+    setState(() {
+      this.category = pred;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Column(
+
+        children: <Widget>[
+          SizedBox(
+            height: 50,
+          ),
+          Center(
+
+            child: _image == null
+                ? Text('Press button to select image.')
+                : Container(
+              constraints: BoxConstraints(
+                  maxHeight: MediaQuery.of(context).size.height / 2),
+              decoration: BoxDecoration(
+                border: Border.all(),
+              ),
+              child: _imageWidget,
+            ),
+          ),
+          SizedBox(
+            height: 36,
+          ),
+          Text(
+            category != null ? category!.label : '',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: Icon(Icons.add_a_photo),
+      ),
+    );
+  }
+}
